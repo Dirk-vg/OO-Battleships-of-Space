8a808f5983454753ce93328eb3150026b0db2256
Dirk van Goethem <vangoethem.dirk@hotmail.com>
2020-07-28 19:23:39 +0200

    added Status

--- lib/Ship.php
+++ lib/Ship.php
@@ -1,13 +1,25 @@
 <?php
 class Ship
 {
-    public $name;
+    private $name;
 
-    public $weaponPower = 0;
+    private $weaponPower = 0;
 
-    public $jediFactor = 0;
+    private $jediFactor = 0;
 
-    public $strength = 0;
+    private $strength = 0;
+
+    private $underRepair;
+
+    public function __construct()
+    {
+        $this->underRepair = mt_rand(1, 100) < 30;
+    }
+
+    public function isFunctional()
+    {
+        return !$this->underRepair;
+    }
 
     public function sayHello()
     {
@@ -44,4 +56,60 @@ class Ship
     {
         return $givenShip->strength > $this->strength;
     }
+
+    public function setStrength($strength)
+    {
+        if (!is_numeric($strength)) {
+            throw new Exception('Invalid strength passed'.$strength);
+        }
+
+        $this->strength = $strength;
+    }
+
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower(): int
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower(int $weaponPower): void
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJediFactor(): int
+    {
+        return $this->jediFactor;
+    }
+
+    /**
+     * @param int $jediFactor
+     */
+    public function setJediFactor(int $jediFactor): void
+    {
+        $this->jediFactor = $jediFactor;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+
 }
